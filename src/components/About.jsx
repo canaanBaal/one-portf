9bfe08982e97f869b556d900d7720309fcd3c576
canaanBaal <johancore16@hotmail.com>
2023-03-31 13:19:06 -0300

    Initial code - Deployment

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+        <p className="text-xl mt-20 mb-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed distinctio totam alias iusto, molestiae iure mollitia, nesciunt repellat fugiat culpa cumque facilis at maiores! Quia facere voluptatum sint enim, esse rerum? Repellat dolore consectetur perferendis, natus culpa provident! Modi totam nisi cumque porro obcaecati ducimus dolorum magni iste minima blanditiis?</p>
+        <br></br>
+        <p className="text-xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo nemo ad vero dolores eos accusantium? Amet culpa corporis fugit autem reprehenderit iusto id? Aut accusamus, quos optio quibusdam similique ipsum, alias id vel officiis voluptatibus ad beatae consequatur veritatis a iusto, facilis unde dolor voluptatum quae quasi impedit minima numquam.</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/NavLinks.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-scroll";
+
+const NavLinks = ({ link, className, onClick }) => {
+  return (
+    <li className={className}>
+        <Link to={link} smooth duration={500} onClick={onClick} >
+            {link}
+        </Link>
+    </li>
+  )
+}
+
+export default NavLinks
